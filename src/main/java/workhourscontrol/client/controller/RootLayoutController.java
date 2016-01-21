5ce94c50df7f0f118132eb98cd763df454655baf
Renato Ribeiro <r3nato.rp+github@gmail.com.br>
2016-01-21 16:52:05 -0200

    adicionanado atalhos para abrir e salvar

--- src/main/java/workhourscontrol/client/controller/RootLayoutController.java
+++ src/main/java/workhourscontrol/client/controller/RootLayoutController.java
@@ -1,11 +1,39 @@
 package workhourscontrol.client.controller;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
 import workhourscontrol.client.MainApp;
 
 public class RootLayoutController {
 
+	@FXML
+	private BorderPane rootPane;
+
 	private MainApp mainApp;
+
+	@FXML
+	public void initialize() {
+
+		 rootPane.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+
+				//Atalho para salvar
+				if (event.isControlDown() && event.getCode() == KeyCode.S) {
+					mainApp.salvar();
+				}
+
+				//Atalho para carregar
+				if (event.isControlDown() && event.getCode() == KeyCode.O) {
+					mainApp.carregar();
+				}
+			}
+		});
+
+	}
 
 	@FXML
 	public void handleExit() {
